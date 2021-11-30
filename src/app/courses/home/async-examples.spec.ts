018113f416f68b026d6733e2f0ded838280b22a3
Leon Grin <41086608+leongrin@users.noreply.github.com>
2021-11-30 16:22:32 -0300

    updates

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,15 +1,14 @@
-
 /*f at the beginning to focus on running only this test => fdescribe*/
-import {fakeAsync, flush, tick} from '@angular/core/testing';
+import {fakeAsync, flush, flushMicrotasks, tick} from '@angular/core/testing';
 
-fdescribe('async testing examples',  () => {
+fdescribe('async testing examples', () => {
 
 
-  it('Asynchronous test example with jasmine done() ',  (done) => {
+  it('Asynchronous test example with jasmine done() ', (done) => {
 
     let test = false;
 
-    setTimeout( () => {
+    setTimeout(() => {
 
       console.log('running assertions');
       test = true;
@@ -22,11 +21,11 @@ fdescribe('async testing examples',  () => {
 
 
   /*The fakeAsync zone detects all asynchronous operations*/
-  it('Asynchronous test example - setTimeout()', fakeAsync (() => {
+  it('Asynchronous test example - setTimeout()', fakeAsync(() => {
 
     let test = false;
 
-    setTimeout( () => {
+    setTimeout(() => {
 
       console.log('running assertions');
       test = true;
@@ -42,6 +41,70 @@ fdescribe('async testing examples',  () => {
 
     expect(test).toBeTruthy();  // with fakeAsync, we don't need to run our assertions inside nested code blocks, which is one advantage
     // over the done alternative.
+
+  }));
+
+
+  /*using the fit utility to focus only on this test*/
+  it('Asynchronous test example - plain promise', fakeAsync(() => {
+
+    let test = false;
+    console.log('Creating promise');
+
+    Promise.resolve().then(() => {
+
+      console.log('Promise first then() evaluated successfully');
+      test = true;
+
+      return Promise.resolve();
+
+    }).then(() => {
+
+      console.log('Promise second then() evaluated successfully');
+
+    });
+
+    flushMicrotasks(); // Microtests are promises, which have priority (run faster) over tasks like setTimeout for example.
+
+    console.log('Running test assertions');
+
+    expect(test).toBeTruthy();
+
+  }));
+
+
+
+  fit('Asynchronous test example - Promises + setTimeout()', fakeAsync (() => {
+
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+
+      counter += 10;
+
+      setTimeout(() => {
+
+        counter += 1;
+
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+
+    flushMicrotasks(); // flushing promises only
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(11);
+
+
+
 
   }));
 
